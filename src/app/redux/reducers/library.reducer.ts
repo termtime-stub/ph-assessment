f8b561072b1c1b8511c3ec526b020f560dde34f5
Mario Mejia <mariofer_mejia@hotmail.com>
2022-02-19 20:56:52 -0600

    feat: add DataPersistenceService

--- src/app/redux/reducers/library.reducer.ts
+++ src/app/redux/reducers/library.reducer.ts
@@ -3,6 +3,7 @@ import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import {LibraryState, LibraryPayload} from "../../../types/state";
 import {RootState} from "../../store";
 import {getNewReleasesAction, searchSpotifyAction} from "./search.reducer";
+import {DataPersistenceService} from "../../../services/DataPersistenceService";
 
 const initialState: LibraryState = {
   loadingLibrary: false,
@@ -26,18 +27,32 @@ interface SaveSongProps {
 export const saveSongAction = createAsyncThunk(
   "library/saveSong",
   async ({user, song}: SaveSongProps, api) => {
-    //TEMP save on redux
-
     const state = api.getState() as RootState;
 
-    const newSongs: Track[] = [
-      {...song, isInLibrary: true},
-      ...state.library.songs,
-    ];
+    const existingSongIndex = state.library.songs.findIndex(
+      (s) => s.id === song.id
+    );
+
+    let modifiedSong;
+    // let newLibrarySongState;
+
+    if (existingSongIndex === -1) {
+      // Song is new, add to firestore
+      modifiedSong = await DataPersistenceService.saveSongInFirestore(
+        song,
+        user
+      );
+    } else {
+      // Song already exists, update state in firestore
+      modifiedSong = await DataPersistenceService.updateSongLibraryAddStatus(
+        song,
+        user,
+        false
+      );
+    }
 
     const response: LibraryPayload = {
-      songs: newSongs,
-      modifiedSong: song,
+      modifiedSong,
     };
 
     return response;
@@ -48,28 +63,29 @@ export const removeSongAction = createAsyncThunk(
   "library/removeSong",
   async ({user, song}: SaveSongProps, api) => {
     const state = api.getState() as RootState;
-
-    const newSongs = [...state.library.songs];
-
-    const indexOfSong = newSongs.findIndex((s) => s.id === song.id);
+    const indexOfSong = state.library.songs.findIndex((s) => s.id === song.id);
 
     if (indexOfSong === -1) {
       // Song not found, send back current state.
+
+      //TODO: get latest state of this song from firestore and send that one back instead, if that fails, then return error
       const response: LibraryPayload = {
-        songs: newSongs,
-        modifiedSong: undefined,
+        modifiedSong: song,
       };
 
       return response;
     }
 
-    const modifiedSong = {...newSongs[indexOfSong], isInLibrary: false};
-
-    newSongs.splice(indexOfSong, 1, modifiedSong);
+    // Sync changes to firestore
+    const modifiedSong =
+      await DataPersistenceService.updateSongLibraryAddStatus(
+        song,
+        user,
+        false
+      );
 
     const response: LibraryPayload = {
-      songs: newSongs,
-      modifiedSong: song,
+      modifiedSong,
     };
 
     return await response;
@@ -84,6 +100,7 @@ const librarySlice = createSlice({
     builder
       .addCase(loadLibrary.pending, (state) => {
         state.loadingLibrary = true;
+        state.error = undefined;
       })
       .addCase(loadLibrary.fulfilled, (state, action) => {
         state.songs = action.payload.songs!;
@@ -91,41 +108,77 @@ const librarySlice = createSlice({
       })
       .addCase(loadLibrary.rejected, (state, action) => {
         state.loadingLibrary = false;
-        state.error = action.error;
+        state.error =
+          "Your library could not be loaded, please try again later.";
       })
       ///
       .addCase(removeSongAction.pending, (state) => {
         state.loadingRemove = true;
+        state.error = undefined;
       })
       .addCase(removeSongAction.fulfilled, (state, action) => {
-        state.songs = action.payload.songs!;
+        const modifiedSong = action.payload.modifiedSong!;
+        const indexOfSong = state.songs.findIndex(
+          (s) => s.id === modifiedSong.id
+        );
+
+        let newSongState;
+        if (indexOfSong === -1) {
+          // If the action resulted in no changes, return same state
+          newSongState = state.songs;
+        } else {
+          // Otherwise update the song in the library
+          newSongState = [...state.songs];
+          newSongState.splice(indexOfSong, 1, modifiedSong);
+        }
+
+        state.songs = newSongState;
         state.loadingRemove = false;
       })
       .addCase(removeSongAction.rejected, (state, action) => {
         state.loadingRemove = false;
-        state.error = action.error;
+        state.error =
+          "Could not remove the song from your library, please try again later.";
       })
       ///
       .addCase(saveSongAction.fulfilled, (state, action) => {
-        state.songs = action.payload.songs!;
-      })
-      .addCase(saveSongAction.rejected, (state, action) => {
-        state.error = action.error;
-      })
-      // Save songs gotten from search and new release to keep track of them
-      .addCase(searchSpotifyAction.fulfilled, (state, action) => {
-        const songsToAdd = action.payload.results.filter(
-          (s) => state.songs.findIndex((s2) => s2.id === s.id) === -1
-        );
-        state.songs = [...songsToAdd, ...state.songs];
-      })
-      .addCase(getNewReleasesAction.fulfilled, (state, action) => {
-        const songsToAdd = action.payload.results.filter(
-          (s) => state.songs.findIndex((s2) => s2.id === s.id) === -1
+        const modifiedSong = action.payload.modifiedSong!;
+
+        const indexOfSong = state.songs.findIndex(
+          (s) => s.id === modifiedSong.id
         );
 
-        state.songs = [...songsToAdd, ...state.songs];
+        let newSongState;
+        if (indexOfSong === -1) {
+          // If the song was just added to the library, add it to the start of the library
+          newSongState = [modifiedSong, ...state.songs];
+        } else {
+          // If the song already existed in the state, move it to the first position
+          newSongState = [...state.songs];
+          newSongState.splice(indexOfSong, 1);
+          newSongState.unshift(modifiedSong);
+        }
+
+        state.songs = newSongState;
+      })
+      .addCase(saveSongAction.rejected, (state, action) => {
+        state.error =
+          "Could not add the song to your library, please try again later.";
       });
+    // Save songs gotten from search and new release to keep track of them
+    // .addCase(searchSpotifyAction.fulfilled, (state, action) => {
+    //   const songsToAdd = action.payload.results.filter(
+    //     (s) => state.songs.findIndex((s2) => s2.id === s.id) === -1
+    //   );
+    //   state.songs = [...songsToAdd, ...state.songs];
+    // })
+    // .addCase(getNewReleasesAction.fulfilled, (state, action) => {
+    //   const songsToAdd = action.payload.results.filter(
+    //     (s) => state.songs.findIndex((s2) => s2.id === s.id) === -1
+    //   );
+
+    //   state.songs = [...songsToAdd, ...state.songs];
+    // });
   },
 });
 
---
+++ src/services/DataPersistenceService.ts
@@ -0,0 +1,66 @@
+import {User} from "@auth0/auth0-react";
+import {FirebaseConnector} from "../app/providers/Firebase";
+
+export class DataPersistenceService {
+  /**
+   * Gets all the songs in the library of a user.
+   *
+   * @param user
+   * @returns
+   */
+  static async getLibraryFromFirestore(user: User): Promise<Track[]> {
+    const connector = await FirebaseConnector.getInstance();
+
+    const queryRes = await connector.db
+      .collection(`libraries/${user.sub}/songs`)
+      .where("isInLibrary", "==", true)
+      .get();
+
+    return queryRes.docs.map((d) => d.data()) as Track[];
+  }
+
+  /**
+   * Used to update whether or not this song is added to the library
+   *
+   * @param track
+   * @param user
+   * @param newStatus
+   * @returns
+   */
+  static async updateSongLibraryAddStatus(
+    track: Track,
+    user: User,
+    newStatus: boolean
+  ): Promise<Track> {
+    const connector = await FirebaseConnector.getInstance();
+
+    const updatedSong: Track = {...track, isInLibrary: newStatus};
+
+    await connector.db
+      .collection(`libraries/${user.sub}/songs`)
+      .doc(track.id)
+      .update({isInLibrary: newStatus});
+
+    return updatedSong;
+  }
+
+  /**
+   * Called when it's the first time we favorite a song.
+   *
+   * Saves the track and marks it as added into the user's library
+   * @param track
+   * @param isFavorited
+   */
+  static async saveSongInFirestore(track: Track, user: User): Promise<Track> {
+    const connector = await FirebaseConnector.getInstance();
+
+    const updatedSong: Track = {...track, isInLibrary: true};
+
+    await connector.db
+      .collection(`libraries/${user.sub}/songs`)
+      .doc(track.id)
+      .set(updatedSong);
+
+    return updatedSong;
+  }
+}
